e97194b74a8d713274d8e4e8555a6db774fd84d6
inwooshin <dkrlehowl45@naver.com>
2021-09-25 17:35:10 +0900

    할일 리사이클러뷰 추가
    - '+' 버튼을 누를 시에 새로운 리사이클러뷰 생성
    - '+' 버튼을 누를 시에 새로운 창이 떠서 습관 세부 내용 설정창 띄움

--- fixed_habit/app/src/main/java/com/inwooshin/fixed_habit/ToDoList.kt
+++ fixed_habit/app/src/main/java/com/inwooshin/fixed_habit/ToDoList.kt
@@ -3,15 +3,26 @@ package com.inwooshin.fixed_habit
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TextView
+import androidx.core.view.marginLeft
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 
 class ToDoList : Fragment() {
+    private var arrayList: ArrayList<MainData>? = null
+    private var mainAdapter: MainAdapter? = null
+    private var recyclerView: RecyclerView? = null
+    private var linearLayoutManager: LinearLayoutManager? = null
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
@@ -25,6 +36,32 @@ class ToDoList : Fragment() {
 
         setup.setOnClickListener {
             val intent = Intent(activity, SetupActivity::class.java)
+            startActivity(intent)
+        }
+
+        var addbtn = rootView.findViewById<FloatingActionButton>(R.id.addButton)
+
+        recyclerView = rootView.findViewById<View>(R.id.add_list_rv) as RecyclerView
+        linearLayoutManager = LinearLayoutManager(activity)
+        recyclerView!!.layoutManager = linearLayoutManager
+
+        arrayList = ArrayList<MainData>()
+
+        mainAdapter = MainAdapter(arrayList)
+        recyclerView!!.adapter = mainAdapter
+        val lm = GridLayoutManager(activity, 2)
+        recyclerView?.layoutManager = lm as RecyclerView.LayoutManager?
+
+        addbtn.setOnClickListener {
+            /*
+            var mainData = MainData(R.drawable.ic_run,"달리기", "PM 7 : 30", "월, 수, 금")
+
+            arrayList!!.add(mainData)
+            mainAdapter!!.notifyDataSetChanged()
+             */
+
+
+            val intent = Intent(activity, AddPropertyActivity::class.java)
             startActivity(intent)
         }
 
