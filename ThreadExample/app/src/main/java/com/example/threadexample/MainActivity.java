5de85ea94cd7430a474b49e996d166b869a74081
inwooshin <dkrlehowl45@naver.com>
2021-12-11 15:03:15 +0900

    thread 예제 작성

---
+++ ThreadExample/app/src/main/java/com/example/threadexample/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.threadexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_stop;
+    Thread thread;
+    boolean isThread = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
+
+        //스레드 시작
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                isThread = true;
+                thread = new Thread(){
+                    public void run(){
+                         while(isThread){
+                              try {
+                                  sleep(5000);
+                              } catch (InterruptedException e) {
+                                  e.printStackTrace();
+                              }
+
+                              handler.sendEmptyMessage(0);
+                         }
+                    }
+                };
+
+                thread.start();
+            }
+        });
+
+        //스레드 종료
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isThread = false;
+            }
+        });
+
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Toast.makeText(getApplicationContext(), "신인우 강의", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+}
